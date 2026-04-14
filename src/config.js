5359a78ab81484a1074b9dff8ef1d6793ecb7b80
f3stoner <252489762+f3stoner@users.noreply.github.com>
2026-04-13 22:41:00 -0500

    Update Old 300 intro template

--- src/config.js
+++ src/config.js
@@ -26,11 +26,27 @@ export const REGION_AOS = {
 }
 
 export const REGION_INTRO_TEMPLATES = {
-    "96c9eef9-3b6e-4365-86cd-51dbeccf231a": [
-        "This is where the introduction template will live!"
-    ],
+    "96c9eef9-3b6e-4365-86cd-51dbeccf231a": `This is where the introduction template will live!`,
 
-    "0925d0c8-2c87-4d9c-882a-86efa0ce1c5a": [
+    "0925d0c8-2c87-4d9c-882a-86efa0ce1c5a": `Welcome to F3
 
-    ],
+F3 Mission: The mission of F3 is to plant, grow and serve small workout groups for men for the invigoration of male community leadership.
+
+5 Core Principles:
+
+1. Free of charge
+2. Open to all men
+3. Held outdoors, rain or shine
+4. Peer led
+5. Always ends in a Circle of Trust
+
+F3 Credo: Leave no man behind, but leave no man where you found him.
+
+Disclaimer: F3 workouts are free and open to all men. Your participation is voluntary. I am not a fitness professional or healthcare provider. F3 workouts involve strenuous physical exercise. Push yourself but don’t hurt yourself. If you feel like something is wrong, let me know. Don’t hesitate to modify your exercise. We all do it. Aye?
+
+ID of FNGs and assignment of battle buddies -
+
+CPR and cell phone check -
+
+Count-O-Rama -`,
 }
